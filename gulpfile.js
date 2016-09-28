6c06809569b372119e44b2ef0ccdbdbaf12dc637
Emily Pepke <elpepke41@gmail.com>
2016-09-28 00:27:47 -0400

    Add build system

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp'),
+  connect = require('gulp-connect'),
+  sass = require('gulp-sass');
+
+gulp.task('webserver', function() {
+  connect.server({
+    livereload: true
+  });
+});
+
+gulp.task('sass', function() {
+  gulp.src('css/style.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('css'))
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+    gulp.watch('styles/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'webserver', 'watch']);
+
