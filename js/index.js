af02aa8ffa6a26094538997257a6369475d5e52b
Emily Pepke <elpepke41@gmail.com>
2016-10-12 22:42:01 -0400

    css and js added, still needs work, but good enough for part 1

---
+++ js/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $("#vote").submit(function(event) {
+    $('#form').addClass("hidden");
+     if ($('input[name=expansion]:checked').length > 0) {
+       var name = $('input[name=expansion]:checked').val();
+       document.getElementById("expPicked").innerHTML = "Response recorded: You voted for " + name + ".";
+    }
+    else {
+      $('#notRecorded').removeClass("hidden");
+    }
+
+    return false;
+  });
+
+  $('.trigger').click(function() {
+    $('.dropdown-menu').toggleClass('visible');
+  });
+});
+
+
+
