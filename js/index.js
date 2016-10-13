f9eda9137916cfc83d10f8cabeecd0369e489979
Emily Pepke <elpepke41@gmail.com>
2016-10-13 15:35:16 -0400

    small jquery style guide fix

--- js/index.js
+++ js/index.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
   $("#vote").submit(function(event) {
     $('#form').addClass("hidden");
      if ($('input[name=expansion]:checked').length > 0) {
-       var name = $('input[name=expansion]:checked').val();
-       document.getElementById("expPicked").innerHTML = "Response recorded: You voted for " + name + ".";
+       var $name = $('input[name=expansion]:checked').val();
+       document.getElementById("expPicked").innerHTML = "Response recorded: You voted for " + $name + ".";
     }
     else {
       $('#notRecorded').removeClass("hidden");
